cf7d5670ee519f50994429c26e42b0afb298750b
Todd Emrich <dtemrich10@gmail.com>
2018-08-22 21:15:11 -0500

    Initial Project

--- Week1/Program.cs
+++ Week1/Program.cs
@@ -7,7 +7,6 @@ namespace Week1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
         }
     }
 }

e98e9f83f7676d0139540ad9b8704c433dffff1b
Todd Emrich <dtemrich10@gmail.com>
2018-08-23 19:06:15 -0500

    Inital checkin

--- Week1/Program.cs
+++ Week1/Program.cs
@@ -7,6 +7,7 @@ namespace Week1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
